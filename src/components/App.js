9efc655aaea474c242a6b0bdbeb3da2986703bad
ketan mehta <ketanmehta244241@gmail.com>
2021-01-10 06:30:21 +0530

    favouriting movies

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,15 @@ class App extends React.Component {
     store.dispatch(addMovies(data));
     console.log("STATE", this.props.store.getState());
   }
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState();
+    const index = favourites.indexOf(movie);
+    if (index !== -1) {
+      // found the movie
+      return true;
+    }
+    return false;
+  };
   render() {
     const { list } = this.props.store.getState(); // {list :[],favourites:[]}
     console.log("RENDER", this.props.store.getState());
@@ -30,7 +39,12 @@ class App extends React.Component {
 
           <div className="list">
             {list.map((movie, index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+              <MovieCard
+                movie={movie}
+                key={`movies-${index}`}
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieFavourite(movie)}
+              />
             ))}
           </div>
         </div>
